e4e3b6088b7325513229ccce54f45de96c1069f2
ComConDevelopment <comcondev@gmail.com>
2014-01-23 11:51:27 +0100

    Lichtsteuerung angefangen

---
+++ SeCon/Models/LightModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeCon.Models
+{
+    public class LightModel : ViewModelBase
+    {
+        #region Deklaration
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Konstruktor
+
+        #endregion
+
+        #region Events
+
+        #endregion
+
+        #region Diverses
+
+        #endregion
+
+
+
+
+    }
+}
